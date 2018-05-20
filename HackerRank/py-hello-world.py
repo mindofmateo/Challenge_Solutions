6a82a5e79ee71519e19fedfb6423d0e9df459af3
mindofmateo <15581904+mindofmateo@users.noreply.github.com>
2018-05-20 15:24:51 -0500

    Added a basic HackerRank solution.

---
+++ HackerRank/py-hello-world.py
@@ -0,0 +1,11 @@
+'''
+
+Problem URL:
+https://www.hackerrank.com/challenges/py-hello-world/problem
+
+Summary: print "Hello, World!" to stdout/the console.
+
+'''
+
+if __name__ == '__main__':
+    print("Hello, World!")
