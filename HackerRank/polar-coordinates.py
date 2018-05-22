aa7aba8ee2c9b60028c7e8f77a5167f12f71d7db
mindofmateo <15581904+mindofmateo@users.noreply.github.com>
2018-05-22 12:27:49 -0500

    Solved a basic math challenge on HackerRank, and updated the HR Python template.

---
+++ HackerRank/polar-coordinates.py
@@ -0,0 +1,17 @@
+'''
+URL:
+https://www.hackerrank.com/challenges/polar-coordinates/problem
+
+Summary:
+Given a complex z, convert it to polar coordinates
+'''
+
+from cmath import polar # , phase
+
+def complexToPolar(complex_coords=input())
+    [print(x) for x in polar(complex(complex_coords))]
+
+if __name__ == '__main__':
+    complexToPolar()
+
+
